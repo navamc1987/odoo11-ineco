b732a0261060479c36c98d07788f42208c97cd55
navamc1987 <128137083+navamc1987@users.noreply.github.com>
2023-07-24 20:43:55 +0700

    Add files via upload

---
+++ ineco_l10n_v11/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010 INECO PARTNERSHIP LIMITED (http://www.ineco.co.th)
+# All Right Reserved
+
---
+++ ineco_l10n_v11/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010 INECO PARTNERSHIP LIMITED (http://www.ineco.co.th)
+# All Right Reserved
+
+
+{
+    'name': 'INECO - CHART OF ACCOUNT',
+    'version': '1.0',
+    'depends': ["account", "ineco_thai_v11"],
+    'author': 'INECO LTD.,PART.',
+    'category': 'Localization/Account Charts',
+    'summary': 'ผังบัญชีไทย',
+    'description': "THAI CHART OF ACCOUNT",
+    'website': 'http://www.ineco.co.th',
+    'data': ['account_data.xml'],
+    'update_xml': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'images': [],
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
